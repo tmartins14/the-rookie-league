2d60c3757e7019945590fa46191d1390617d1075
tmartins14 <martinstyler14@gmail.com>
2022-09-19 08:36:03 -0400

    Adding data to table

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,77 +4,52 @@ import KEY from '../apis/apiKey';
 import '../input.css';
 
 const Home = () => {
-  // // const [gameWeek, setGameWeek] = useState('');
-  // const [results, setResults] = useState([]);
-  // const [season, setSeason] = useState('2022REG');
-  // const [week, setWeek] = useState(1);
-  // // const KEY = KEY;
+  // const [gameWeek, setGameWeek] = useState('');
+  const [results, setResults] = useState([]);
+  const [season, setSeason] = useState('2022REG');
+  const [week, setWeek] = useState(1);
+  // const KEY = KEY;
 
-  // useEffect(() => {
-  //   const scores = async () => {
-  //     const { data } = await axios.get(
-  //       `https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/${season}/${week}`,
-  //       {
-  //         params: {
-  //           key: KEY
-  //         }
-  //       }
-  //     );
-  //     console.log(data);
-  //     setResults(data);
-  //   };
-  //   scores();
-  // }, []);
+  useEffect(() => {
+    const scores = async () => {
+      const { data } = await axios.get(
+        `https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/${season}/${week}`,
+        {
+          params: {
+            key: KEY
+          }
+        }
+      );
+      // console.log(data);
+      setResults(data);
+    };
+    scores();
+  }, []);
 
   // console.log(results);
   return (
     <div className="overflow-hidden bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">
-          Applicant Information
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          Personal details and application.
-        </p>
+        <h3 className="text-lg font-medium leading-6 text-gray-900">Scores</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">Week 1</p>
       </div>
       <div className="border-t border-gray-200">
         <dl>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Full name</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              Margot Foster
+          <div className="px-4 py-5 sm:grid sm:grid-cols-5 justify-around">
+            <dd className="text-sm font-medium text-gray-500">
+              {results[0].HomeTeam}
             </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Application for
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              Backend Developer
+            <dd className="text-sm font-medium text-gray-500">
+              {results[0].HomeScore}
             </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Email address</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              margotfoster@example.com
+            <dd className="text-sm font-medium text-gray-500">
+              {results[0].GameEndDateTime}
             </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Salary expectation
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              $120,000
+            <dd className="text-sm font-medium text-gray-500">
+              {results[0].AwayScore}
             </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-              incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-              consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-              proident. Irure nostrud pariatur mollit ad adipisicing
-              reprehenderit deserunt qui eu.
+            <dd className="text-sm font-medium text-gray-500">
+              {results[0].AwayTeam}
             </dd>
           </div>
         </dl>
