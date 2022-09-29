846781f1ba2003e1cfaccc017a2fe8228c744412
tmartins14 <martinstyler14@gmail.com>
2022-09-29 15:42:12 -0400

    Started styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,40 +16,44 @@ const Home = () => {
   const [selected, setSelected] = useState(1);
   const [results, setResults] = useState([]);
   const [season, setSeason] = useState('2022REG');
+  // const [defaultWeek, setDefaultWeek] = useState(true);
 
   // Get Current Week - Default
   // useEffect(() => {
-  //   const currentWeek = async () => {
+  //   if (defaultWeek) {
+  //     const currentWeek = async () => {
+  //       const { data } = await axios.get(
+  //         `https://api.sportsdata.io/v3/nfl/scores/json/CurrentWeek`,
+  //         {
+  //           params: {
+  //             key: KEY
+  //           }
+  //         }
+  //       );
+  //       setSelected(data);
+  //     };
+  //     currentWeek();
+  //     setDefaultWeek(false);
+  //   }
+  // }, []);
+
+  // console.log('Current Week:', selected);
+
+  // // Get Current Season - Default
+  // useEffect(() => {
+  //   const currentSeason = async () => {
   //     const { data } = await axios.get(
-  //       `https://api.sportsdata.io/v3/nfl/scores/json/CurrentWeek`,
+  //       `https://api.sportsdata.io/v3/nfl/scores/json/CurrentSeason`,
   //       {
   //         params: {
   //           key: KEY
   //         }
   //       }
   //     );
-  //     setSelected(data);
+  //     setSeason(data);
   //   };
-  //   currentWeek();
+  //   currentSeason();
   // }, []);
-
-  // console.log('Current Week:', selected);
-
-  // Get Current Season - Default
-  useEffect(() => {
-    const currentSeason = async () => {
-      const { data } = await axios.get(
-        `https://api.sportsdata.io/v3/nfl/scores/json/CurrentSeason`,
-        {
-          params: {
-            key: KEY
-          }
-        }
-      );
-      setSeason(data);
-    };
-    currentSeason();
-  }, []);
 
   // Pull Scores for Selected season and week
   useEffect(() => {
@@ -96,21 +100,31 @@ const Home = () => {
 
   // console.log(results);
   return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">Scores</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">Week {selected}</p>
+    <section className="pt-20 lg:pt-[120px] pb-10 lg:pg-20 bg-[#acc5fa]">
+      <div className="container">
+        <div className="overflow-hidden sm:rounded-lg w-full md:w-2/3 xl:w-2/3 px-4">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Scores
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Week {selected}
+            </p>
+          </div>
+          <div>
+            <Dropdown
+              label="Select a Week"
+              selected={selected}
+              onSelectedChange={setSelected}
+              options={weekOptions}
+            />
+            <div className="border-t bg-white border-gray-200 shadow sm:rounded-lg">
+              {gameScores}
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <Dropdown
-          label="Select a Week"
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={weekOptions}
-        />
-        <div className="border-t border-gray-200">{gameScores}</div>
-      </div>
-    </div>
+    </section>
   );
 };
 
